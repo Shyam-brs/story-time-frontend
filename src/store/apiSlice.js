597f111b713274451da962f6f72809befe32cf6a
Shyam sundhar Kunduru <123252647+shyamsundharkunduru-brs@users.noreply.github.com>
2024-06-13 17:01:22 +0530

    url-upt

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000",
+  baseUrl: "https://story-time-backend.onrender.com",
   prepareHeaders: (headers, { getState }) => {
     const state = getState();
- 
+
     const token = state.auth.token;
     if (token) {
       headers.set("authorization", `Bearer ${token}`);
