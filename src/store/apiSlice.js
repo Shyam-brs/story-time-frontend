feaa0882a4e499d058ffd0cbbcdf525522557cac
Shyam sundhar Kunduru <123252647+shyamsundharkunduru-brs@users.noreply.github.com>
2024-06-14 09:24:15 +0530

    upt-4

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "/api/users",
+  baseUrl: "https://story-time-backend.onrender.com",
   prepareHeaders: (headers, { getState }) => {
     const state = getState();
 
